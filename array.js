2c5946dd5f94f5694e19017181813345e189cd18
ashraful-islam-siam <ashrafullislam.dev1@gmail.com>
2024-08-31 00:51:03 +0600

    Learning about js array

---
+++ array.js
@@ -0,0 +1,13 @@
+const numbers = [10, 20, 30, 50, 30, 5, 40];
+const friends = [
+  "Humayun Faridi",
+  "Razzak",
+  "Siam Ahmed",
+  "Alamgir",
+  "Bulbul Ahmed",
+];
+const fruits = ["Apple", "Orange", "Watermelon", "Strawberry", "Mango"];
+
+console.log("Number:", numbers);
+console.log("Friends", friends);
+console.log("Fruits:", fruits);
---
+++ length.js
@@ -0,0 +1,13 @@
+const numbers = [10, 20, 30, 50, 30, 5, 40];
+const friends = [
+  "Humayun Faridi",
+  "Razzak",
+  "Siam Ahmed",
+  "Alamgir",
+  "Bulbul Ahmed",
+];
+const fruits = ["Apple", "Orange", "Watermelon", "Strawberry", "Mango"];
+
+console.log("Length of numbers:", numbers.length);
+console.log("Length of friends:", friends.length);
+console.log("Length of fruits:", fruits.length);
